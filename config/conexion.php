ab08f18341f5cedcdfe4644c17096fce428c26ee
Jose <josepena2196@gmail.com>
2019-11-15 05:14:45 -0600

    Cambio en el nombre de la base de datos

--- config/conexion.php
+++ config/conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$conexion = new mysqli('localhost','root','','veterinaria3');
+	$conexion = new mysqli('localhost','root','','proveterinaria');
 	if($conexion->connect_errno){
 		echo "Error de conexion a la base de datos";
 	}else{
