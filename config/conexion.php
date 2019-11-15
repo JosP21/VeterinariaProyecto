820ea55ec92e4479bc2c74b14b520edbc4491c59
JosP21 <51894571+JosP21@users.noreply.github.com>
2019-11-14 20:51:39 -0600

    archivo de configuracion

---
+++ config/conexion.php
@@ -0,0 +1,8 @@
+<?php
+	$conexion = new mysqli('localhost','root','','veterinaria3');
+	if($conexion->connect_errno){
+		echo "Error de conexion a la base de datos";
+	}else{
+		//echo "Conectado";
+	}
+?>
