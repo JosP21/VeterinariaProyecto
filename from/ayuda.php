75dc16f7adb7604e766386ee330a48bcf3f5d7aa
MonicaAlvarado98 <57803730+MonicaAlvarado98@users.noreply.github.com>
2019-11-25 10:09:33 -0600

    Update ayuda.php

--- from/ayuda.php
+++ from/ayuda.php
@@ -1,6 +1,6 @@
 <HTML>
 <head>
-	<title>PDF-Ayudaaaaaaaaaaaaaaaaaaaaaaaaaaaa</title>
+	<title>PDF-Ayuda</title>
 </head>
 <body style="background-color: #6c6c6c;" onload="abrirVentana();">
 <embed src="../assets/doc/ayuda.pdf" type="application/pdf" style="width: 100%;margin-top: 0;" width="100%" height="100%">
