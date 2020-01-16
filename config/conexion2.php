2907c32dc67799835e2ee6db151ec0050c432f69
Josué Cornejo <josue1415@yahoo.es>
2020-01-15 22:46:04 -0600

    conexiones

--- config/conexion2.php
+++ config/conexion2.php
@@ -1,8 +1,8 @@
 <?php
-	$conexion = new mysqli('localhost','root','','veterinaria5');
-	if($conexion->connect_errno){
-		echo "Error de conexion a la base de datos";
-	}else{
+$conexion = new mysqli('localhost','comuesco_cc16125','iMlK^,=z7XiO','comuesco_cc16125');
+if($conexion->connect_errno){
+	echo "Error de conexion a la base de datos";
+}else{
 		//echo "Conectado";
-	}
+}
 ?>
