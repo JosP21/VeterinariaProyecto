c12c6603f942d812f02714580e31856f22ed1988
Josué Cornejo <josue1415@yahoo.es>
2020-01-16 13:16:59 -0600

    conexiones

--- config/conexion.php
+++ config/conexion.php
@@ -1,5 +1,6 @@
 <?php
-$conexion = new mysqli('localhost','comuesco_cc16125','iMlK^,=z7XiO','comuesco_cc16125');
+$conexion = new mysqli('localhost','comuesco_animalpets','%,+]H3(ivIl!','comuesco_animalpets');
+$acentos = $conexion->query("SET NAMES 'utf8'");
 if($conexion->connect_errno){
 	echo "Error de conexion a la base de datos";
 }else{
